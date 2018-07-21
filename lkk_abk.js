8b5ab50eac2d75dcb923f21d551a8a58c4f68b66
ai-takeuchi <41495689+ai-takeuchi@users.noreply.github.com>
2018-07-21 16:33:56 +0900

    Add files via upload

--- lkk_abk.js
+++ lkk_abk.js
@@ -7,6 +7,7 @@ var lkk_groups = {};
 var lkk_addrs = [];
 
 (function () {
+
 	var Address = {
 		to: "",
 		to_array: [],
@@ -24,7 +25,7 @@ var lkk_addrs = [];
 		}
 		// console.log(contact_ids);
 
-		var trs = `<tr><th> </th><th><input type="checkbox" group="all" value="to" id="to_all" /><label for="to_all">To</label><input type="checkbox" group="all" value="cc" id="cc_all" /><label for="cc_all">Cc</label><input type="checkbox" group="all" value="bcc" id="bcc_all" /><label for="bcc_all">Bcc</label></th></tr>`;
+		var trs = '<tr><th> </th><th><input type="checkbox" group="all" value="to" id="to_all" /><label for="to_all">To</label><input type="checkbox" group="all" value="cc" id="cc_all" /><label for="cc_all">Cc</label><input type="checkbox" group="all" value="bcc" id="bcc_all" /><label for="bcc_all">Bcc</label></th></tr>';
 		var group_id = 0;
 		for (var i in lkk_addrs) {
 			var contact = lkk_addrs[i];
@@ -44,7 +45,7 @@ var lkk_addrs = [];
 
 				var status = [];
 				var regexp = getMatchEmailRegexp(email);
-				["to", "cc", "bcc"].forEach(toccbcc => {
+				["to", "cc", "bcc"].forEach(function (toccbcc) {
 					status[toccbcc] = 'checked="checked"';
 					if (Address[toccbcc].match(regexp) === null) {
 						status[toccbcc] = '';
@@ -56,18 +57,18 @@ var lkk_addrs = [];
 				}
 				// console.log(emails);
 
-				emails += `<input type="checkbox" group="${group_id}" value="to" id="to_${group_id}" ${status["to"]} /><label for="to_${group_id}">To</label>`;
+				emails += '<input type="checkbox" group="' + group_id + '" value="to" id="to_' + group_id + '" ' + status["to"] + ' /><label for="to_' + group_id + '">To</label>';
 
-				emails += `<input type="checkbox" group="${group_id}" value="cc" id="cc_${group_id}" ${status["cc"]} /><label for="cc_${group_id}">Cc</label>`;
+				emails += '<input type="checkbox" group="' + group_id + '" value="cc" id="cc_' + group_id + '" ' + status["cc"] + ' /><label for="cc_' + group_id + '">Cc</label>';
 
-				emails += `<input type="checkbox" group="${group_id}" value="bcc" id="bcc_${group_id}" ${status["bcc"]} /><label for="bcc_${group_id}">Bcc</label>`;
+				emails += '<input type="checkbox" group="' + group_id + '" value="bcc" id="bcc_' + group_id + '" ' + status["bcc"] + ' /><label for="bcc_' + group_id + '">Bcc</label>';
 
-				emails += ` <span class="email" group="${group_id}" contact_id="${contact_id}" email_index="${email_index}">${email}</span>`;
+				emails += ' <span class="email" group="' + group_id + '" contact_id="' + contact_id + '" email_index="' + email_index + '">' + email + '</span>';
 			}
 			// console.log(emails);
-			trs += `<tr><td class="contact_name">${contact_name}</td><td>${emails}</td></tr>`;
+			trs += '<tr><td class="contact_name">' + contact_name + '</td><td>' + emails + '</td></tr>';
 		}
-		var table = `<table>${trs}</table>`;
+		var table = '<table>' + trs + '</table>';
 		return table;
 	}
 
@@ -76,24 +77,24 @@ var lkk_addrs = [];
 	}
 
 	function get_group_html() {
-		var options = `<option value="0">${rcmail.gettext('all', 'lkk_addressbook')}</option>`;
+		var options = '<option value="0">' + rcmail.gettext('all', 'lkk_addressbook') + '</option>';
 		for (var id in lkk_groups) {
 			// console.log(lkk_groups[id]);
 			var group = lkk_groups[id];
 			if (is_exists_contact_id(group) == false) {
 				continue;
 			}
-			options += `<option value="${id}">${group["name"]}</option>`;
+			options += '<option value="' + id + '">' + group["name"] + '</option>';
 		}
-		return `<select id="lkk_groups">${options}</select>`;
+		return '<select id="lkk_groups">' + options + '</select>';
 	}
 
 
 	function is_exists_contact_id(group) {
 		var found = false;
-		group["contact_id"].forEach(contact_id => {
+		group["contact_id"].forEach(function (contact_id) {
 			for (var i in lkk_addrs) {
-				// console.log(`${group["name"]} ${lkk_addrs[i]["contact_id"]} == ${contact_id}`);
+				// console.log('' + group["name"] + '' + lkk_addrs[i]["contact_id"] + '== ' + contact_id + '');
 				if (lkk_addrs[i]["contact_id"] == contact_id) {
 					found = true;
 				}
@@ -129,18 +130,18 @@ var lkk_addrs = [];
 			return;
 		}
 
-		$('body').append(`<div class="lkk_addressbook">
-			<header>
-				<div class="toolbar">
-					<button class="close" title="${rcmail.gettext('close', 'lkk_addressbook')}">&times;</button>
-					<span class="group"></span>
-					<button class="done">${rcmail.gettext('done', 'lkk_addressbook')}</button>
-				</div>
-			</header>
-			<div class="addrs_wrap">
-				<div class="addrs"></div>
-			</div>
-		</div>`);
+		$('body').append('<div class="lkk_addressbook">' +
+			'<header>' +
+			'<div class="toolbar">' +
+			'<button class="close" title="' + rcmail.gettext('close', 'lkk_addressbook') + '">&times;</button>' +
+			'<span class="group"></span>' +
+			'<button class="done">' + rcmail.gettext('done', 'lkk_addressbook') + '</button>' +
+			'</div>' +
+			'</header>' +
+			'<div class="addrs_wrap">' +
+			'<div class="addrs"></div>' +
+			'</div>' +
+			'</div>');
 
 		$('.lkk_addressbook .group').html(get_group_html());
 		$('.lkk_addressbook .addrs').html(get_address_html());
@@ -168,14 +169,14 @@ var lkk_addrs = [];
 			var status = self.prop('checked');
 
 			if (group === "all") {
-				$(`.lkk_addressbook input`).prop('checked', false);
-				$(`.lkk_addressbook input[value="${toccbcc}"]`).prop('checked', status);
+				$('.lkk_addressbook input').prop('checked', false);
+				$('.lkk_addressbook input[value="' + toccbcc + '"]').prop('checked', status);
 
-				$(`.lkk_addressbook .email`).each(function () {
+				$('.lkk_addressbook .email').each(function () {
 					var e = $(this);
 					var email_index = e.attr("email_index");
 					var contact_id = e.attr("contact_id");
-					// console.log(`contact_id: ${contact_id}`);
+					// console.log('contact_id: ' + contact_id}');
 					var contact = get_contact(contact_id, email_index);
 					// console.log(contact);
 					set_address(contact["name"], contact["email"], "to", toccbcc == "to" ? status : false);
@@ -183,13 +184,13 @@ var lkk_addrs = [];
 					set_address(contact["name"], contact["email"], "bcc", toccbcc == "bcc" ? status : false);
 				});
 			} else {
-				$(`.lkk_addressbook input[group="${group}"]`).prop('checked', false);
+				$('.lkk_addressbook input[group="' + group + '"]').prop('checked', false);
 				self.prop('checked', status);
 
-				var e = $(`.lkk_addressbook .email[group="${group}"]`);
+				var e = $('.lkk_addressbook .email[group="' + group + '"]');
 				var email_index = e.attr("email_index");
 				var contact_id = e.attr("contact_id");
-				// console.log(`contact_id: ${contact_id}`);
+				// console.log('contact_id: ' + contact_id + '');
 				var contact = get_contact(contact_id, email_index);
 				// console.log(contact);
 				set_address(contact["name"], contact["email"], "to", toccbcc == "to" ? status : false);
@@ -209,14 +210,14 @@ var lkk_addrs = [];
 			// console.log("not found");
 			if (status) {
 				// add address
-				Address[toccbcc + '_array'].push(`${name} <${email}>`);
+				Address[toccbcc + '_array'].push('' + name + ' <' + email + '>');
 			}
 		} else {
 			// console.log("found");
 			if (!status) {
 				// console.log("remove");
 				// remove address
-				for (const key in Address[toccbcc + '_array']) {
+				for (var key in Address[toccbcc + '_array']) {
 					if (Address[toccbcc + '_array'][key].match(regexp) === null) {
 						continue;
 					}
