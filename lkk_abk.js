10f4339eaa8f7e1d963dd5412634ec6f63b89f4b
ai-takeuchi <41495689+ai-takeuchi@users.noreply.github.com>
2018-07-21 10:25:34 +0900

    Add files via upload

--- lkk_abk.js
+++ lkk_abk.js
@@ -1,14 +1,217 @@
+/*
+ * lkk_abk.js
+ *
+ */
 
-function show_lkk(target){
-	if($.inArray(target,['_to','_cc','_bcc'])==-1){
-		return false
+(function () {
+	var Address = {
+		to: "",
+		to_array: [],
+		cc: "",
+		cc_array: [],
+		bcc: "",
+		bcc_array: [],
+	};
+
+	function get_address_html() {
+		var group_id = $('#lkk_groups').val();
+		var contact_ids = undefined;
+		if (lkk_groups[group_id] !== undefined) {
+			contact_ids = lkk_groups[group_id]["contact_id"];
+		}
+		// console.log(contact_ids);
+
+		var trs = `<tr><th> </th><th><input type="checkbox" group="all" value="to" id="to_all" /><label for="to_all">To</label><input type="checkbox" group="all" value="cc" id="cc_all" /><label for="cc_all">Cc</label><input type="checkbox" group="all" value="bcc" id="bcc_all" /><label for="bcc_all">Bcc</label></th></tr>`;
+		var group_id = 0;
+		for (var i in lkk_addrs) {
+			var contact = lkk_addrs[i];
+			var contact_id = contact["contact_id"];
+			var contact_name = contact["name"];
+
+			// group member?
+			if (contact_ids !== undefined && contact_ids.indexOf(contact_id) < 0) {
+				continue;
+			}
+
+			var emails = "";
+			for (var email_index in contact["email"]) {
+				group_id++;
+
+				var email = contact["email"][email_index];
+
+				var status = [];
+				var regexp = getMatchEmailRegexp(email);
+				["to", "cc", "bcc"].forEach(toccbcc => {
+					status[toccbcc] = 'checked="checked"';
+					if (Address[toccbcc].match(regexp) === null) {
+						status[toccbcc] = '';
+					}
+				});
+
+				if (emails) {
+					emails += "<br>";
+				}
+				// console.log(emails);
+
+				emails += `<input type="checkbox" group="${group_id}" value="to" id="to_${group_id}" ${status["to"]} /><label for="to_${group_id}">To</label>`;
+
+				emails += `<input type="checkbox" group="${group_id}" value="cc" id="cc_${group_id}" ${status["cc"]} /><label for="cc_${group_id}">Cc</label>`;
+
+				emails += `<input type="checkbox" group="${group_id}" value="bcc" id="bcc_${group_id}" ${status["bcc"]} /><label for="bcc_${group_id}">Bcc</label>`;
+
+				emails += ` <span class="email" group="${group_id}" contact_id="${contact_id}" email_index="${email_index}">${email}</span>`;
+			}
+			// console.log(emails);
+			trs += `<tr><td class="contact_name">${contact_name}</td><td>${emails}</td></tr>`;
+		}
+		var table = `<table>${trs}</table>`;
+		return table;
 	}
-	window.open('./?_task=mail&_action=plugin.lkk_abk_show&t='+target,'lkk','height=500,width=550,scrollbars=yes,focus=yes');
-}
 
-rcmail.addEventListener('init',function(evt){
-	var link=$('.title label');
-	link.click(function(){
-		show_lkk($(this).attr('for'))
+	function getMatchEmailRegexp(email) {
+		return new RegExp('([\\s<,;:]+|^)(' + email.replace(".", "\\.") + ')([\\s>,;:]+|$)', 'i');
+	}
+
+	function get_group_html() {
+		var options = `<option value="0">${rcmail.gettext('all', 'lkk_addressbook')}</option>`;
+		for (var id in lkk_groups) {
+			// console.log(lkk_groups[id]);
+			var group = lkk_groups[id];
+			options += `<option value="${id}">${group["name"]}</option>`;
+		}
+		return `<select id="lkk_groups">${options}</select>`;
+	}
+
+	function get_contact(contact_id, email_index) {
+		for (var i in lkk_addrs) {
+			var contact = lkk_addrs[i];
+			if (contact["contact_id"] != contact_id) {
+				continue;
+			}
+			return { name: contact["name"], email: contact["email"][email_index] };
+		}
+	}
+
+	function lkk_init() {
+
+		Address["to"] = $('textarea[name="_to"]').val();
+		Address["to_array"] = Address["to"].split(/\s*,\s*/);
+		Address["cc"] = $('textarea[name="_cc"]').val();
+		Address["cc_array"] = Address["cc"].split(/\s*,\s*/);
+		Address["bcc"] = $('textarea[name="_bcc"]').val();
+		Address["bcc_array"] = Address["bcc"].split(/\s*,\s*/);
+
+		if ($('.lkk_addressbook')[0]) {
+			$('.lkk_addressbook').show();
+			return;
+		}
+
+		$('body').append(`<div class="lkk_addressbook">
+			<header>
+				<div class="toolbar">
+					<span class="group"></span>
+					<button class="done">${rcmail.gettext('done', 'lkk_addressbook')}</button>
+					<button class="close">${rcmail.gettext('close', 'lkk_addressbook')}</button>
+				</div>
+			</header>
+			<div class="addrs_wrap">
+				<div class="addrs"></div>
+			</div>
+		</div>`);
+
+		$('.lkk_addressbook .group').html(get_group_html());
+		$('.lkk_addressbook .addrs').html(get_address_html());
+
+		$(document).on('click', '.lkk_addressbook .close', function () {
+			close_dialog();
+		});
+
+		$(document).on('change', '#lkk_groups', function () {
+			$('.lkk_addressbook .addrs').html(get_address_html());
+		});
+
+		$(document).on('click', '.lkk_addressbook .done', function () {
+			$('textarea[name="_to"]').val(Address["to"]);
+			$('textarea[name="_cc"]').val(Address["cc"]);
+			$('textarea[name="_bcc"]').val(Address["bcc"]);
+			close_dialog();
+		});
+
+		// to, cc, bcc
+		$(document).on('change', '.lkk_addressbook input[type="checkbox"]', function () {
+			var self = $(this);
+			var group = self.attr("group");
+			var toccbcc = self.val();
+			var status = self.prop('checked');
+
+			if (group === "all") {
+				$(`.lkk_addressbook input`).prop('checked', false);
+				$(`.lkk_addressbook input[value="${toccbcc}"]`).prop('checked', status);
+
+				$(`.lkk_addressbook .email`).each(function () {
+					var e = $(this);
+					var email_index = e.attr("email_index");
+					var contact_id = e.attr("contact_id");
+					// console.log(`contact_id: ${contact_id}`);
+					var contact = get_contact(contact_id, email_index);
+					// console.log(contact);
+					set_address(contact["name"], contact["email"], "to", toccbcc == "to" ? status : false);
+					set_address(contact["name"], contact["email"], "cc", toccbcc == "cc" ? status : false);
+					set_address(contact["name"], contact["email"], "bcc", toccbcc == "bcc" ? status : false);
+				});
+			} else {
+				$(`.lkk_addressbook input[group="${group}"]`).prop('checked', false);
+				self.prop('checked', status);
+
+				var e = $(`.lkk_addressbook .email[group="${group}"]`);
+				var email_index = e.attr("email_index");
+				var contact_id = e.attr("contact_id");
+				// console.log(`contact_id: ${contact_id}`);
+				var contact = get_contact(contact_id, email_index);
+				// console.log(contact);
+				set_address(contact["name"], contact["email"], "to", toccbcc == "to" ? status : false);
+				set_address(contact["name"], contact["email"], "cc", toccbcc == "cc" ? status : false);
+				set_address(contact["name"], contact["email"], "bcc", toccbcc == "bcc" ? status : false);
+			}
+		});
+	}
+
+	function close_dialog() {
+		$('.lkk_addressbook').hide();
+	}
+
+	function set_address(name, email, toccbcc, status) {
+		var regexp = getMatchEmailRegexp(email)
+		if (Address[toccbcc].match(regexp) === null) {
+			// console.log("not found");
+			if (status) {
+				// add address
+				Address[toccbcc + '_array'].push(`${name} <${email}>`);
+			}
+		} else {
+			// console.log("found");
+			if (!status) {
+				// console.log("remove");
+				// remove address
+				for (const key in Address[toccbcc + '_array']) {
+					if (Address[toccbcc + '_array'][key].match(regexp) === null) {
+						continue;
+					}
+					// console.log('!!! ' + Address[toccbcc + '_array'][key]);
+					Address[toccbcc + '_array'].splice(key, 1);
+					// delete Address[toccbcc + '_array'][key];
+				}
+			}
+		}
+		// console.log(Address[toccbcc + '_array']);
+		Address[toccbcc] = (Address[toccbcc + '_array'].join(", ").replace(/,[\s,]*/g, ", ").replace(/(^[\s,]*|[\s,]*$)/g, "") + ", ").replace(/^[\s,]*$/, "");
+		// console.log(Address[toccbcc]);
+	}
+
+	rcmail.addEventListener('init', function (event) {
+		$(document).on('click', '.title label', function () {
+			lkk_init();
+		});
 	});
-});
+
+})();
--- lkk_addressbook.php
+++ lkk_addressbook.php
@@ -1,138 +1,99 @@
 <?php
 
-class lkk_addressbook extends rcube_plugin{
+class lkk_addressbook extends rcube_plugin {
 	public $task = 'mail';
 	public $action = 'compose';
-	private $global_address = false;
+	private $global_address = true; // false;
 	private $addrs = array();
 	private $groups = array();
 
-	function init(){
+	function init() {
+        //echo 'test init';
 		$rcmail = rcmail::get_instance();
-		if ($rcmail->action == 'compose'){
+		if ($rcmail->action == 'compose') {
 			$this->include_script('lkk_abk.js');
 			$this->include_stylesheet("skins/default/templates/lkk.css");
 		}
 
-		$this->add_texts('localization/');
+		// output footer
+		$rcmail->output->add_footer($this->output_footer());
+
+		$this->add_texts('localization/', true);
 		$this->register_action('plugin.lkk_abk_show',array($this,'show'));
-		$this->register_handler('plugin.target',array($this,'get_t'));
-		$this->register_handler('plugin.act_title',array($this,'set_t'));
-		$this->register_handler('plugin.table',array($this,'tab'));
-		$this->set_groups();
+
+		// $this->register_handler('plugin.target',array($this,'get_t'));
+		// $this->register_handler('plugin.act_title',array($this,'set_t'));
+		// $this->register_handler('plugin.table',array($this,'tab'));
+
+		// $this->add_hook('hook-name', $callback);
 	}
 
-	function show(){
-		if (get_input_value('t', RCUBE_INPUT_GET) !== null) {
-			$addr=rcmail::get_instance()->get_address_book('sql');
-			$addr->set_pagesize(9999);
-			//$rt=$addr->list_records(array('name','email','contact_id'));
-			$rt=$addr->list_records(array('name','email'));
-			//var_dump($rt);
-			while($u=$rt->next()){
-				//var_dump($u);
-				$this->addrs[]=array('name'=>$u['name'],'email'=>$u['email'],'contact_id'=>$u['contact_id']);
-				//$this->addrs[]=array('name'=>$u['name'],'email'=>$u['email']);
-			}
-
-			//Global AddressBook
-			if($this->global_address===true){
-				$addr=rcmail::get_instance()->get_address_book('global');
-				$addr->set_pagesize(9999);
-				//$rt=$addr->list_records(array('name','email','contact_id'));
-				$rt=$addr->list_records(array('name','email'));
-				while($u=$rt->next()){
-					$this->addrs[]=array('name'=>$u['name'],'email'=>$u['email'],'contact_id'=>$u['contact_id']);
-					//$this->addrs[]=array('name'=>$u['name'],'email'=>$u['email']);
-				}
-			}
-			//var_dump($this->addrs);
-			
-			$output=rcmail::get_instance()->output;
-			$output->set_pagetitle($this->gettext('title'));
-			$output->send('lkk_addressbook.mini_book');
-		}
+	function output_footer() {
+		$ret = "<script>\n";
+		$ret .= "/* aic-tokai */\n";
+		$ret .= 'var lkk_groups = '.json_encode($this->get_groups()).";\n";
+		$ret .= 'var lkk_addrs = '.json_encode($this->get_addresses()).";\n";
+		$ret .= "</script>\n";
+		return $ret;
 	}
 
-	function get_t(){
-		return get_input_value('t', RCUBE_INPUT_GET);
-	}
-
-	function set_t(){
-		//return $this->gettext('add_to').$this->gettext($this->get_t());
-		return $this->gettext('add_to');//.$this->gettext($this->get_t());
-	}
-
-	function set_groups(){
-		$rcmail = rcmail::get_instance();
-		$db = $rcmail->db;
-		$user_id = $rcmail->user->data['user_id'];
- 		$sql_result = $db->query("SELECT * FROM contactgroups WHERE del=0 and user_id = ? order by name", $user_id);
-		while ($sql_arr = $db->fetch_assoc($sql_result)) {
-		      $this->groups[] = $sql_arr;
-		}
-		//var_dump($this->groups);
-	}
-
-	function get_group_select_html() {
-		 $html = '<div class="group_selector_div"><span>Group: </span><select name="group_selector">';
-		 $html .= '<option value="-1">All</option>';
-		 foreach ($this->groups as $key => $v) {
-		 	 $html .= '<option value="' . $v['contactgroup_id'] . '">' . $v['name'] . '</option>';
-		 }
-		 $html .= '</select>';
-		 $html .= ' <input type="button" name="lkk_button_select_all_to" value="To" />';
-		 $html .= '<input type="button" name="lkk_button_select_all_to_off" value="to" />';
-		 $html .= ' <input type="button" name="lkk_button_select_all_cc" value="Cc" />';
-		 $html .= '<input type="button" name="lkk_button_select_all_cc_off" value="cc" />';
-		 $html .= ' <input type="button" name="lkk_button_select_all_bcc" value="Bcc" />';
-		 $html .= '<input type="button" name="lkk_button_select_all_bcc_off" value="bcc" /></div>';
-		 return $html;
-	}
-	
-	function get_group_ids($contact_id){
-		$group_ids = array();
-		$rcmail = rcmail::get_instance();
-		$db = $rcmail->db;
-		//echo ' contact_id='.$contact_id;
- 		$sql_result = $db->query("SELECT * FROM contactgroupmembers WHERE contact_id = ?", $contact_id);
-		//var_dump($sql_result);
-		while ($sql_arr = $db->fetch_assoc($sql_result)) {
-		      //print_r($sql_arr);
-		      $group_ids[] = $sql_arr['contactgroup_id'];
-		}
-		//var_dump($group_ids);
-		return $group_ids;
-	}
-
-	function get_group_id_classes($group_ids) {
-		//var_dump($group_ids);
-		$class = 'group_id_' . join(' group_id_', $group_ids);
-		$class = preg_replace('/(group_id_$|group_id_ )/', '', $class);
-		//echo '['.$class.']';
-		return $class;
+    /**
+     * get users data
+     */
+    function get_addresses() {
+        $addr=rcmail::get_instance()->get_address_book('sql');
+        $addr->set_pagesize(9999);
+        // $rt=$addr->list_records(array('name','email','contact_id'));
+        $rt=$addr->list_records(array('name','email'));
+        // var_dump($rt);
+        while($u=$rt->next()){
+            // var_dump($u);
+            $this->addrs[]=array('name'=>$u['name'],'email'=>$u['email'],'contact_id'=>$u['contact_id']);
+            // $this->addrs[]=array('name'=>$u['name'],'email'=>$u['email']);
         }
 
-	function tab(){
-		$html=$this->get_group_select_html();
-		$addr=$this->addrs;
-		$html.='<table id="addrs"><tr><th>'.$this->gettext('name').'</th><th>'.$this->gettext('mail').'</th></tr>'."\n";
-		for($n=0;$n<count($addr);$n++){
-			//print_r($addr);
-			$group_classes = $this->get_group_id_classes($this->get_group_ids($addr[$n]['contact_id']));
-			$html.='<tr class="group_id_tr '.$group_classes.'"><td>'.$addr[$n]['name'].'</td><td>';
-			for($m=0;$m<count($addr[$n]['email']);$m++){
-				//if ($m > 0) { $html.='<br />'; }
-				$html.='<div class="email_address"><input class="mail_addr_to" type="checkbox" />';
-				$html.='<input class="mail_addr_cc" type="checkbox" />';
-				$html.='<input class="mail_addr_bcc" type="checkbox" />';
-				$html.='<span class="mail_addr">'.$addr[$n]['email'][$m].'</span></div>';
-			}
-			$html.="</td></tr>\n";
+        // Global AddressBook
+        if ($this->global_address === true) {
+            $addr=rcmail::get_instance()->get_address_book('global');
+            $addr->set_pagesize(9999);
+            //$rt=$addr->list_records(array('name','email','contact_id'));
+			$rt=$addr->list_records(array('name','email'));
+			$addrs = array();
+            while ($u=$rt->next()) {
+                $addrs[]=array('name'=>$u['name'],'email'=>$u['email'],'contact_id'=>$u['contact_id']);
+                //$this->addrs[]=array('name'=>$u['name'],'email'=>$u['email']);
+            }
 		}
-		$html.='</table>';
-		return $html;
+		return $addrs;
+        //var_dump($this->addrs);
 	}
-}
 
-?>
+    /**
+     * get groups data
+     */
+    function get_groups(){
+        $global_addressbook_user_id = 0;
+		if ($this->global_address !== true){
+		    $addr = rcmail::get_instance()->get_address_book('global');
+		    $global_groups = $addr->list_groups();
+		    var_dump($addr->list_groups());
+            $a = $addr->list_groups();
+            $global_addressbook_user_id = $a[0]['user_id'];
+		}
+        $rcmail = rcmail::get_instance();
+        //var_dump($rcmail);
+        $db = $rcmail->db;
+        $user_id = $rcmail->user->data['user_id'];
+        // exclude global address book with global address book user_id if disabled.
+		$sql_result = $db->query("SELECT * FROM contactgroups,contactgroupmembers WHERE contactgroups.contactgroup_id = contactgroupmembers.contactgroup_id AND user_id != ?", $global_addressbook_user_id);
+		$groups = array();
+        while ($sql_arr = $db->fetch_assoc($sql_result)) {
+            //echo $sql_arr['contactgroup_id'];
+            $groups[$sql_arr['contactgroup_id']]['name'] = $sql_arr['name'];
+            $groups[$sql_arr['contactgroup_id']]['contact_id'][] = $sql_arr['contact_id'];
+        }
+		//var_dump($this->groups);
+		return $groups;
+	}
+
+}
